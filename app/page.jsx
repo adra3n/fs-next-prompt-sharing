25b4b26dd8a9905eab0ed7e77144753eaeaaf610
adra3n <srtc.kcgl@gmail.com>
2023-08-02 13:04:12 +0300

    homepage wip

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden"></br>
+        <span className="orange_gradient text-center">
+          Your Prompts AI-Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        In this app you can share your most creative prompts with other users
+        and discover new ones
+      </p>
+    </section>
+  )
 }
 
 export default Home
