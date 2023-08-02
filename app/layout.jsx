b4c56cb77610dda7a62ee2a983a8f9831d6af3ff
adra3n <srtc.kcgl@gmail.com>
2023-08-02 13:48:27 +0300

    desktop nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@styles/globals.css'
-import { Children } from 'react'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
   title: 'Prompt Share',
@@ -13,7 +14,11 @@ const RootLayout = ({ children }) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          <Provider />
+          {children}
+        </main>
       </body>
     </html>
   )
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import { signIn, signOut, useSession, getProviders } from 'next-auth/next'
 
 const Nav = () => {
-  return <div>Nav</div>
+  const isUserLoggedIn = true
+  const [providers, setProviders] = useState(null)
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders()
+      setProviders(response)
+    }
+    setProviders()
+  }, [])
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Prompt Share"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Prompt Share</p>
+      </Link>
+      {/* Large Device Nav */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/profile.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile image"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  className="black_btn"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  )
 }
 
 export default Nav
