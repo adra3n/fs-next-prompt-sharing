05375d84dcbb86ccb94e0a8cdaa6d96ce23d2b80
adra3n <srtc.kcgl@gmail.com>
2023-08-02 19:08:28 +0300

    db connect and api keys

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,23 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+})
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: '',
-      clientSecret: '',
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+    } catch (error) {}
+  },
 })
 
-export default route
+export { handler as GET, handler as POST }
