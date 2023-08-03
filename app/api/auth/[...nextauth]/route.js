f85f25cfeaf647f645b75c06fe2b52611d95967e
adra3n <srtc.kcgl@gmail.com>
2023-08-03 15:12:44 +0300

    auth callbacks fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,32 +14,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    })
-    session.user.id = sessionUser._id.toString()
-    return session
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB()
-      // check user exists
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       })
-      if (!userExists) {
-        await User.create({
+      session.user.id = sessionUser._id.toString()
+      return session
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB()
+        // check user exists
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace('', '').toLowerCase(),
-          image: profile.picture,
         })
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace('', '').toLowerCase(),
+            image: profile.picture,
+          })
+        }
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
       }
-      return true
-    } catch (error) {
-      console.log(error)
-      return false
-    }
+    },
   },
 })
 
