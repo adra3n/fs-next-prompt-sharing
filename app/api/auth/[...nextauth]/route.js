d3281fe561c57ec22647c2029f5e92ad9d47e6eb
adra3n <srtc.kcgl@gmail.com>
2023-08-03 15:57:14 +0300

    auth cb problem revert

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,34 +14,32 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    async session({ session }) {
-      const sessionUser = await User.findOne({
-        email: session.user.email,
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    })
+    session.user.id = sessionUser._id.toString()
+    return session
+  },
+  async signIn({ profile }) {
+    try {
+      await connectToDB()
+      // check user exists
+      const userExists = await User.findOne({
+        email: profile.email,
       })
-      session.user.id = sessionUser._id.toString()
-      return session
-    },
-    async signIn({ profile }) {
-      try {
-        await connectToDB()
-        // check user exists
-        const userExists = await User.findOne({
+      if (!userExists) {
+        await User.create({
           email: profile.email,
+          username: profile.name.replace('', '').toLowerCase(),
+          image: profile.picture,
         })
-        if (!userExists) {
-          await User.create({
-            email: profile.email,
-            username: profile.name.replace('', '').toLowerCase(),
-            image: profile.picture,
-          })
-        }
-        return true
-      } catch (error) {
-        console.log(error)
-        return false
       }
-    },
+      return true
+    } catch (error) {
+      console.log(error)
+      return false
+    }
   },
 })
 
