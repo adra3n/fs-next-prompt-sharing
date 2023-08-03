c6f8e8a3a05af508424265306ef5ca3c3f5e73f0
adra3n <srtc.kcgl@gmail.com>
2023-08-03 13:54:38 +0300

    signIn create

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { connectToDB } from '@utils/database'
+import User from '@models/user'
 console.log({
   clientId: process.env.GOOGLE_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -17,7 +18,17 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       await connectToDB()
-
+      // check user exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      })
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace('', '').toLowerCase(),
+          image: profile.picture,
+        })
+      }
       return true
     } catch (error) {
       console.log(error)
