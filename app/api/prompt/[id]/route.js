b71419e51db81c27005cb4ea1b06e7ea18d13b1d
adra3n <srtc.kcgl@gmail.com>
2023-08-05 17:45:59 +0300

    delete prompt with id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -37,3 +37,19 @@ export const PATCH = async (request, { params }) => {
     return new Response('Failed to update the prompt', { status: 500 })
   }
 }
+
+//DELETE
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB()
+    const existingPrompt = await Prompt.findById(params.id)
+    if (!existingPrompt) {
+      return new Response('Prompt not found', { status: 404 })
+    }
+    await findByIdAndRemove(params.id)
+    return new Response('Prompt deleted successfully', { status: 200 })
+  } catch (error) {
+    return new Response('Failed to delete the prompt', { status: 500 })
+  }
+}
